b62049ee9ad7c7c1a2372ce5a668df49f38f1f74
mehrab-shakib <fatscorpieeth@gmail.com>
2024-08-02 18:37:41 +0600

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+
+const Hero = () => {
+  return (
+    <section  className="w-full nav-height bg-black relative">
+    <div className="h-5/6 w-full flex-center flex-col">
+    <p id="hero" className="hero-title">
+      iPhone 15 Pro
+    </p>
+    </div>
+      
+    </section>
+  )
+}
+
+export default Hero
